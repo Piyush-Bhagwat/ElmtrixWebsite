ed78ae6d50dd90a5f930ef183a8b47c8879a3903
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2022-12-15 23:17:25 +0530

    added mobile responsive version 1

--- Views/logic.js
+++ Views/logic.js
@@ -13,3 +13,32 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".navLinks");
+    const navLinks = document.querySelectorAll(".navLinks li");
+    const body = document.querySelector('body');
+    const htmlEL = document.querySelector('html');
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("navigationActive");
+        body.classList.toggle("stuck");
+        htmlEL.classList.toggle("stuck");
+
+        // Animate
+        navLinks.forEach((links, index) => {
+            if (links.style.animation) {
+                links.style.animation = '';
+            } else {
+                links.style.animation = `navLinkFade 0.7s ease  forwards ${index/7 + 0.5}s`;
+            }
+        });
+
+        //Burger Anim
+        burger.classList.toggle('toggle');
+    });  
+}
+
+navSlide();
